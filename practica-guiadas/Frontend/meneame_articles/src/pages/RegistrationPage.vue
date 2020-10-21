bb3ed2edcc6e4984e03234464c7de815af131790
satoshix13 <satoshix13@gmail.com>
2020-10-21 17:13:41 +0200

    register func added

--- practica-guiadas/Frontend/meneame_articles/src/pages/RegistrationPage.vue
+++ practica-guiadas/Frontend/meneame_articles/src/pages/RegistrationPage.vue
@@ -2,7 +2,7 @@
   
   <div class="login-container container d-flex align-items-center justify-content-center">
 
-    <form class="card p-5">
+    <form class="card p-5" @submit.prevent="register">
 
         <div class="text-center">
           <h3 class="mb-5 font-weight-light">Formulario de registro</h3>
@@ -10,18 +10,23 @@
 
         <div class="form-group">
           <label for="exampleInputEmail1">Nombre de usuario</label>
-          <input type="email" class="form-control" placeholder="usuario" required>
+          <input v-model="user.name" type="text" class="form-control" placeholder="usuario" required>
         </div>
         
         <div class="form-group">
           <label for="exampleInputEmail1">Email</label>
-          <input type="email" class="form-control" placeholder="email" required>
+          <input v-model="user.email" type="email" class="form-control" placeholder="email" required>
         </div>
 
         <div class="form-group">
           <label for="exampleInputEmail1">Contraseña</label>
-          <input type="password" class="form-control" placeholder="contraseña" required>
+          <input v-model="user.password" type="password" class="form-control" placeholder="contraseña" required>
         </div>
+
+
+        <br>
+
+
 
         <div class="row text-center">
         <div class="col-6 col-12-small">
@@ -32,7 +37,7 @@
         
         <div class="col-6 col-12-small">
           <ul class="actions">
-            <li><button type="submit" class="btn btn-secondary mx-auto mt-5">Login</button></li>
+            <li><button  class="btn btn-secondary mx-auto mt-5" @click="toLogin">Login</button></li>
           </ul>
         </div>
       </div>
@@ -45,6 +50,29 @@
 
 <script>
 export default {
-  name: "RegistrationPage"
+  name: "RegistrationPage",
+  data() {
+    return {
+      user: {
+        name: "",
+        email: "",
+        password:""
+      }
+    }
+  },
+  methods:{
+   async register(){
+      try {
+        await this.$http.post("/users", this.user) 
+        alert("Congrats, you account has just been created. Please login ")
+        this.$router.push({name: "login"})
+      }catch(err){
+        console.log("ups, something went wrong")
+      }
+    },
+    toLogin(){
+      this.$router.push({ name: "login"})
+    }
+  }
 }
 </script>
